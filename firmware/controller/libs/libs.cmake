c877bd1e4be75033f63a595664b4bbef26a115e7
MultiMote <contact@mmote.ru>
2025-11-13 16:05:22 +0300

    CMake loop

--- firmware/controller/libs/libs.cmake
+++ firmware/controller/libs/libs.cmake
@@ -2,12 +2,8 @@ include_directories(libs/cmsis-core/Include)
 include_directories(libs/cmsis-device-f1/Include)
 include_directories(libs/stm32f1xx-hal-driver/Inc)
 
-list(APPEND OTHER_SOURCES
-    libs/stm32f1xx-hal-driver/Src/stm32f1xx_ll_adc.c
-    libs/stm32f1xx-hal-driver/Src/stm32f1xx_ll_dma.c
-    libs/stm32f1xx-hal-driver/Src/stm32f1xx_ll_gpio.c
-    libs/stm32f1xx-hal-driver/Src/stm32f1xx_ll_rcc.c
-    libs/stm32f1xx-hal-driver/Src/stm32f1xx_ll_tim.c
-    libs/stm32f1xx-hal-driver/Src/stm32f1xx_ll_usart.c
-    libs/stm32f1xx-hal-driver/Src/stm32f1xx_ll_utils.c
-)
+set(LL_MODULES adc dma gpio rcc tim usart utils)
+
+foreach(mod IN LISTS LL_MODULES)
+    list(APPEND OTHER_SOURCES libs/stm32f1xx-hal-driver/Src/stm32f1xx_ll_${mod}.c)
+endforeach()
