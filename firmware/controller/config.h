b236919552e8a71627295ab61cd47b11316d4ae3
MultiMote <contact@mmote.ru>
2025-11-16 22:36:33 +0300

    Tests

---
+++ firmware/controller/config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+// value 30ml-220ml
+
+// 7g or 11g
+#define GRINDER_MIN_MS 4500
+// ?g
+#define GRINDER_MED_MS 6500
+// ?g
+#define GRINDER_MAX_MS 7800 // or 9000?
+
+#define PRE_WATERING_MS 3000
+
+#endif // CONFIG_H_
