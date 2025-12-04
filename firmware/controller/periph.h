a6b3731b9148c1e901268239d83520169c337c8d
MultiMote <contact@mmote.ru>
2025-12-04 22:02:43 +0300

    Test uart, water count

--- firmware/controller/periph.h
+++ firmware/controller/periph.h
@@ -2,9 +2,13 @@
 #define PERIPH_H_
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
+#include <stdlib.h>
 
 #include "utils.h"
+
+#define UART_BAUD 9600UL
 
 enum be_motor_dir_t
 {
@@ -13,11 +17,43 @@ enum be_motor_dir_t
     BE_MOT_DIR_REV
 };
 
+static volatile uint32_t water_counter = 0;
+
 static const io_def_t be_motor_fwd_io  = IO_BUILD(C, 6);
 static const io_def_t be_motor_rev_io  = IO_BUILD(C, 7);
 static const io_def_t be_cam_top_io    = IO_BUILD(A, 5);
 static const io_def_t be_cam_bottom_io = IO_BUILD(B, 0);
 static const io_def_t be_cam_press_io  = IO_BUILD(A, 6);
+static const io_def_t flow_io          = IO_BUILD(B, 2);
+
+ISR(INT2_vect)
+{
+    water_counter++;
+}
+
+void uart_init()
+{
+    uint8_t ubrr = (F_CPU / (16 * UART_BAUD)) - 1;
+    // Set baud rate
+    UBRRH = (uint8_t)(ubrr >> 8);
+    UBRRL = (uint8_t)ubrr;
+
+    // Enable transmitter
+    UCSRB = _BV(TXEN);
+
+    // Set frame format: 8 data bits, 1 stop bit
+    UCSRC = _BV(URSEL) | _BV(UCSZ1) | _BV(UCSZ0);
+}
+
+void uart_print(const char *str)
+{
+    while (*str)
+    {
+        while (!(UCSRA & (1 << UDRE)));
+        UDR = *str++;
+    }
+}
+
 
 static inline void hw_init(void)
 {
@@ -27,6 +63,30 @@ static inline void hw_init(void)
     configure_input(be_cam_top_io);
     configure_input(be_cam_bottom_io);
     configure_input(be_cam_press_io);
+    configure_input(flow_io);
+
+    uart_init();
+
+    // Trigger INT2 on falling edge
+    MCUCSR &= ~_BV(ISC2);
+    // Enable INT2 interrupt
+    GICR |= _BV(INT2);
+    // Enable global interrupts
+    sei();
+
+    uart_print("hw_init done\r\n");
+
+    char buf[32];
+
+    while (1)
+    {
+        _delay_ms(1000);
+        uart_print("Water count: ");
+        itoa(water_counter, buf, 10);
+        uart_print(buf);
+        uart_print("\r\n");
+    }
+
 }
 
 static inline uint8_t is_cam_top_pressed(void)
